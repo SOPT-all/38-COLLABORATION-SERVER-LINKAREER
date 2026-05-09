21f6c9f3cfecae77309079d9536acd2f528c4d51
hamtorygoals <yhaemin0531@naver.com>
2026-05-09 16:52:38 +0900

    :tada: start: Start New Project

---
+++ src/test/java/org/sopt/soptlinkareer/SoptLinkareerApplicationTests.java
@@ -0,0 +1,11 @@
+package org.sopt.soptlinkareer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SoptLinkareerApplicationTests {
+
+  @Test
+  void contextLoads() {}
+}
