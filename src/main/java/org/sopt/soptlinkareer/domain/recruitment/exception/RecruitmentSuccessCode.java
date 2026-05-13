f7bc82549395cdbfddec8db67adaf17cb9617d41
laura-jung <166522604+laura-jung@users.noreply.github.com>
2026-05-14 01:46:58 +0900

    ✨ feat: 공고 상세 조회 successCode 추가

--- src/main/java/org/sopt/soptlinkareer/domain/recruitment/exception/RecruitmentSuccessCode.java
+++ src/main/java/org/sopt/soptlinkareer/domain/recruitment/exception/RecruitmentSuccessCode.java
@@ -4,7 +4,8 @@ import org.sopt.soptlinkareer.global.exception.code.BaseSuccessCode;
 import org.springframework.http.HttpStatus;
 
 public enum RecruitmentSuccessCode implements BaseSuccessCode {
-  GET_RECRUITMENTS_SUCCESS(HttpStatus.OK, "RECRUITMENT2001", "채용공고 목록 조회에 성공했습니다.");
+  GET_RECRUITMENTS_SUCCESS(HttpStatus.OK, "RECRUITMENT2001", "채용공고 목록 조회에 성공했습니다."),
+  GET_RECRUITMENT_DETAIL(HttpStatus.OK, "RECRUITMENT_2002", "채용공고 상세 조회 성공");
 
   private final HttpStatus httpStatus;
   private final String code;
